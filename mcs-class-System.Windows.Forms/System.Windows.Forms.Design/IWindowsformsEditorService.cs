5f32812b3ef33b20ebba1f8b2f68aa5425ddd0d0
Miguel de Icaza <miguel@gnome.org>
2005-01-30 01:25:13 +0000

    Moved the now deprecated System.Windows.Forms
    svn path=/trunk/old-code/; revision=39771

---
+++ mcs-class-System.Windows.Forms/System.Windows.Forms.Design/IWindowsformsEditorService.cs
@@ -0,0 +1,16 @@
+//
+// System.Windows.Forms.Design.IWindowsFormsEditorService.cs
+// 
+// Author:
+//  Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+// 
+
+namespace System.Windows.Forms.Design
+{
+	public interface IWindowsFormsEditorService
+	{
+		void CloseDropDown ();
+		void DropDownControl (Control control);
+		DialogResult ShowDialog (Form dialog);
+	}
+}
